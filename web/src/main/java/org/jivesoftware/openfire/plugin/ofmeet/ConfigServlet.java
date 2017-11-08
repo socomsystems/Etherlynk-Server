e3a35e28f1d8d5c32ec7b105495a2058b983d0c7
Dele Olajide <dele@uk2.net>
2017-11-08 23:32:40 +0000

    Fixing typo websockets url
    Fixing typo in connectionurl that stop websockets from working

--- web/src/main/java/org/jivesoftware/openfire/plugin/ofmeet/ConfigServlet.java
+++ web/src/main/java/org/jivesoftware/openfire/plugin/ofmeet/ConfigServlet.java
@@ -283,7 +283,7 @@ public class ConfigServlet extends HttpServlet
                 websocketScheme = "ws";
             }
 
-            return new URI( websocketScheme, null, request.getServerName(), request.getServerPort(), "/ws", null, null);
+            return new URI( websocketScheme, null, request.getServerName(), request.getServerPort(), "/ws/", null, null);
         }
         else
         {
